dfdbac12a05ff12e90ff184d80112330f3546e95
Chrisley304 <chrisley304@hotmail.com>
2022-04-03 17:07:15 -0500

    Updated pf

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,126 +1,125 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-if(navToggle){
-    navToggle.addEventListener('click', ()=>{
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-if(navClose){
-    navClose.addEventListener('click',()=>{
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show-menu')
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*==================== ACCORDION SKILLS ====================*/
 
-const skillsContent = document.getElementsByClassName('skills__content'),
-    skillsHeader = document.querySelectorAll('.skills__header')
+const skillsContent = document.getElementsByClassName("skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
 
-function toggleSkills(){
-    let itemClass = this.parentNode.className
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
 
-    for(i = 0; i<skillsContent.length; i++){
-        skillsContent[i].className = 'skills__content skills__close'
-    }
-    if(itemClass === 'skills__content skills__close'){
-        this.parentNode.className = 'skills__content skills__open'
-    }
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
 }
 
-skillsHeader.forEach((el) =>{
-    el.addEventListener('click',toggleSkills)
-})
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
 
 /*==================== QUALIFICATION TABS ====================*/
 
-const tabs = document.querySelectorAll('[data-target]'),
-    tabContents = document.querySelectorAll('[data-content]')
+const tabs = document.querySelectorAll("[data-target]"),
+  tabContents = document.querySelectorAll("[data-content]");
 
-tabs.forEach(tab => {
-    tab.addEventListener('click', () =>{
-        const target = document.querySelector(tab.dataset.target)
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
 
-        tabContents.forEach(tabContent => {
-            tabContent.classList.remove('qualification__active')
-        })
-        target.classList.add('qualification__active')
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
+    });
+    target.classList.add("qualification__active");
 
-        tabs.forEach(tab =>{
-            tab.classList.remove('qualification__active')
-        })
-        tab.classList.add('qualification__active')
-    })
-})
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+    tab.classList.add("qualification__active");
+  });
+});
 
 /*==================== SERVICES MODAL ====================*/
 
-const modalViews = document.querySelectorAll('.services__modal'),
-    modalBtns = document.querySelectorAll('.services__button'),
-    modalCloses = document.querySelectorAll('.services__modal-close')
+const modalViews = document.querySelectorAll(".services__modal"),
+  modalBtns = document.querySelectorAll(".services__button"),
+  modalCloses = document.querySelectorAll(".services__modal-close");
 
-let modal = function(modalClick){
-    modalViews[modalClick].classList.add('active-modal')
-}
+let modal = function (modalClick) {
+  modalViews[modalClick].classList.add("active-modal");
+};
 
-modalBtns.forEach((modalBtn,i) => {
-    modalBtn.addEventListener('click',()=>{
-        modal(i)
-    })
-})
+modalBtns.forEach((modalBtn, i) => {
+  modalBtn.addEventListener("click", () => {
+    modal(i);
+  });
+});
 
 modalCloses.forEach((modalClose) => {
-    modalClose.addEventListener('click',()=>{
-        modalViews.forEach((modalView) =>{
-            modalView.classList.remove('active-modal')
-        })
-    })
-})
+  modalClose.addEventListener("click", () => {
+    modalViews.forEach((modalView) => {
+      modalView.classList.remove("active-modal");
+    });
+  });
+});
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
 let swiper = new Swiper(".portfolio__container", {
-    cssMode: true,
-    loop: false,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable:true,
-    },
+  cssMode: true,
+  loop: false,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
 
 /*==================== CONTACT ME BUTTON ====================*/
 
 var submitMessage = document.getElementById("submitButton"),
-    ContactForm = document.getElementById("ContactForm");
+  ContactForm = document.getElementById("ContactForm");
 
-submitMessage.addEventListener('click',()=>{
-    ContactForm.submit()
-})
+submitMessage.addEventListener("click", () => {
+  ContactForm.submit();
+});
 
 /*==================== TESTIMONIAL ====================*/
-
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
@@ -147,7 +146,7 @@ function scrollActive() {
 }
 window.addEventListener("scroll", scrollActive);
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
 function scrollHeader() {
   const nav = document.getElementById("header");
@@ -157,7 +156,7 @@ function scrollHeader() {
 }
 window.addEventListener("scroll", scrollHeader);
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
 function scrollUp() {
   const scrollup = document.getElementById("scroll-up");
@@ -167,40 +166,35 @@ function scrollUp() {
 }
 window.addEventListener("scroll", scrollUp);
 
-/*==================== DARK LIGHT THEME ====================*/ 
+/*==================== DARK LIGHT THEME ====================*/
 
 const themeButton = document.getElementById("theme-button");
 const darkTheme = "dark-theme";
 const iconTheme = "uil-sun";
 
-
 // We obtain the current theme that the interface has by validating the dark-theme class
-function getCurrentTheme (){
-    return document.body.classList.contains(darkTheme) ? "dark" : "light";
+function getCurrentTheme() {
+  return document.body.classList.contains(darkTheme) ? "dark" : "light";
 }
 
-function getCurrentIcon(){
-    return themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+function getCurrentIcon() {
+  return themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
 }
 
-function ActivateDarkTheme(){
-
-    // Validates if dark theme is not already active 
-    if(getCurrentTheme() != "dark"){
-        document.body.classList.toggle(darkTheme);
-        themeButton.classList.toggle(iconTheme);
-    }
-
+function ActivateDarkTheme() {
+  // Validates if dark theme is not already active
+  if (getCurrentTheme() != "dark") {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+  }
 }
 
-function ActivateLightTheme(){
-
-    // Validates if dark theme is not already active 
-    if(getCurrentTheme() != "light"){
-        document.body.classList.toggle(darkTheme);
-        themeButton.classList.toggle(iconTheme);
-    }
-
+function ActivateLightTheme() {
+  // Validates if dark theme is not already active
+  if (getCurrentTheme() != "light") {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+  }
 }
 
 // Activate / deactivate the theme manually with the button
@@ -210,46 +204,41 @@ themeButton.addEventListener("click", () => {
   themeButton.classList.toggle(iconTheme);
 });
 
-
 // Detect the device mode
 
-    // Check to see if Media-Queries are supported
+// Check to see if Media-Queries are supported
 if (window.matchMedia) {
   // Check if the dark-mode Media-Query matches
-  if(window.matchMedia('(prefers-color-scheme: dark)').matches){
-    ActivateDarkTheme()
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    ActivateDarkTheme();
   } else {
-    ActivateLightTheme()
+    ActivateLightTheme();
   }
 }
 
 if (window.matchMedia) {
   var colorSchemeQuery = window.matchMedia("(prefers-color-scheme: dark)");
-  colorSchemeQuery.addEventListener(
-    "change",()=>{
-        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-          ActivateDarkTheme();
-        } else {
-          ActivateLightTheme();
-        }
-    })
-  ;
+  colorSchemeQuery.addEventListener("change", () => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      ActivateDarkTheme();
+    } else {
+      ActivateLightTheme();
+    }
+  });
 }
 
 if (window.matchMedia) {
   var colorSchemeQuery = window.matchMedia("(prefers-color-scheme: light)");
-  colorSchemeQuery.addEventListener(
-    "change",()=>{
-        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-          ActivateDarkTheme();
-        } else {
-          ActivateLightTheme();
-        }
-    })
-  ;
+  colorSchemeQuery.addEventListener("change", () => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      ActivateDarkTheme();
+    } else {
+      ActivateLightTheme();
+    }
+  });
 }
 
-/*==================== Project Lootie ====================*/ 
+/*==================== Project Lootie ====================*/
 var animation = bodymovin.loadAnimation({
   container: document.getElementById("animContainer"),
   renderer: "svg",
@@ -263,6 +252,8 @@ var animation = bodymovin.loadAnimation({
 const modalView = document.getElementById("success-modal"),
   closeButton = document.getElementById("success__modal-button");
 
-closeButton.addEventListener('click',()=>{
-    modalView.classList.add('hide-modal')
-})
+closeButton.addEventListener("click", () => {
+  modalView.classList.add("hide-modal");
+});
+
+
